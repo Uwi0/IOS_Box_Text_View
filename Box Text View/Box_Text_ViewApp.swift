3a0cb7414bf0bae6731bbc05a6a081945b85fb38
uwi0 <kakapoweb@gmail.com>
2024-08-30 15:33:40 +0700

    Initial Commit

---
+++ Box Text View/Box_Text_ViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Box_Text_ViewApp.swift
+//  Box Text View
+//
+//  Created by dwi prasetyo on 30/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Box_Text_ViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
